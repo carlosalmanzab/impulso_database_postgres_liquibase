04f9de39ef8804529da5a692f89da5b5f3016843
calmanzab <64765773+carlosalmanzab@users.noreply.github.com>
2024-03-14 22:24:08 -0500

    feat(scheme): :art: add initial state for inpulso bd

---
+++ src/main/java/com/database/impulso_database_postgres_liquibase/ImpulsoDatabasePostgresLiquibaseApplication.java
@@ -0,0 +1,13 @@
+package com.database.impulso_database_postgres_liquibase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImpulsoDatabasePostgresLiquibaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImpulsoDatabasePostgresLiquibaseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/database/impulso_database_postgres_liquibase/ImpulsoDatabasePostgresLiquibaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.database.impulso_database_postgres_liquibase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImpulsoDatabasePostgresLiquibaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
